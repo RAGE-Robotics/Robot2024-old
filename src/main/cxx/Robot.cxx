9cacbbf02cd29217b7f3ce978b7cb040e2ee4a1f
Caleb Heydon <probablyme@caleb.engineer>
2023-10-19 13:09:05 -0400

    Added base code

--- src/main/cxx/Robot.cxx
+++ src/main/cxx/Robot.cxx
@@ -1,6 +1,46 @@
 #include "Robot.hxx"
 
+Robot::Robot()
+{
+}
+
+Robot::~Robot()
+{
+}
+
+void Robot::RobotInit()
+{
+}
+
+void Robot::RobotPeriodic()
+{
+}
+
+void Robot::DisabledInit()
+{
+}
+
+void Robot::DisabledPeriodic()
+{
+}
+
+void Robot::AutonomousInit()
+{
+}
+
+void Robot::AutonomousPeriodic()
+{
+}
+
+void Robot::TeleopInit()
+{
+}
+
+void Robot::TeleopPeriodic()
+{
+}
+
 int main(int argc, char **argv)
 {
-    return 0;
+    return frc::StartRobot<Robot>();
 }
--- src/main/include/Robot.hxx
+++ src/main/include/Robot.hxx
@@ -1,5 +1,22 @@
 #pragma once
 
-class Robot
+#include <frc/TimedRobot.h>
+
+class Robot : public frc::TimedRobot
 {
+public:
+    Robot();
+    ~Robot();
+
+    void RobotInit() override;
+    void RobotPeriodic() override;
+
+    void DisabledInit() override;
+    void DisabledPeriodic() override;
+
+    void AutonomousInit() override;
+    void AutonomousPeriodic() override;
+
+    void TeleopInit() override;
+    void TeleopPeriodic() override;
 };
